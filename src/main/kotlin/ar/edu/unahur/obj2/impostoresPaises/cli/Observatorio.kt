d031d81e0a1d9195a914e38ef670a1048d3b7770
Tomas <tomasbenjamin117@gmail.com>
2022-07-03 19:26:31 -0300

    :construction: necesitanTraducción

--- src/main/kotlin/ar/edu/unahur/obj2/impostoresPaises/cli/Observatorio.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/impostoresPaises/cli/Observatorio.kt
@@ -1,7 +1,11 @@
 package ar.edu.unahur.obj2.impostoresPaises.cli
 
 object observatorio {
-    fun sonLimítrofes(pais: Pais, otroPais: Pais) {
-        pais.esLimitrofeA(otroPais)
+    fun sonLimítrofes(pais: Pais, otroPais: Pais): Boolean {
+        return pais.esLimitrofeA(otroPais)
+    }
+
+    fun necesitanTraducción(pais: Pais, otroPais: Pais): Boolean {
+        return true
     }
 }
