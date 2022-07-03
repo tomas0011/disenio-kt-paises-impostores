0f776c2b8b008c899db9475ab4cba777d8129a0f
Tomas <tomasbenjamin117@gmail.com>
2022-07-03 20:03:20 -0300

    :sparkles: obtener5TopPaises

--- src/main/kotlin/ar/edu/unahur/obj2/impostoresPaises/cli/Observatorio.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/impostoresPaises/cli/Observatorio.kt
@@ -1,6 +1,12 @@
 package ar.edu.unahur.obj2.impostoresPaises.cli
 
 object observatorio {
+    val paises = mutableListOf<Pais>()
+
+    fun agregarPais(pais: Pais): Unit {
+        paises.add(pais)
+    }
+
     fun sonLimítrofes(pais: Pais, otroPais: Pais): Boolean {
         return pais.sonLimitrofes(otroPais)
     }
@@ -19,5 +25,10 @@ object observatorio {
 
     fun aCuántoEquivale(pais: Pais, otroPais: Pais, monto: Double): Double {
         return pais.aCuantoEquivale(otroPais, monto)
+    }
+
+    fun obtener5TopPaises(): List<Pais> {
+        paises.sortByDescending { it.densidadPoblacional() }
+        return paises.subList(0, 5)
     }
 }
