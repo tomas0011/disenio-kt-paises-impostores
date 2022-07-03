c2802c141f30d88f49c7a8086f8ed308539ef7e3
LautaroRiofrio <riofriolautaro@gmail.com>
2022-07-03 18:11:51 -0300

    crear clase pais

---
+++ src/main/kotlin/ar/edu/unahur/obj2/impostoresPaises/cli/Pais.kt
@@ -0,0 +1,19 @@
+package ar.edu.unahur.obj2.impostoresPaises.cli
+
+
+
+
+class Pais (
+  nombre: String,
+  codigoISO: String,
+  poblacion: Int,
+  superficie: Double,
+  continente: String,
+  codigoMoneda: String,
+  cotizacion: Double,
+  paisesLimitrofes: List<Pais>,
+  bloquesRegionales: List<String>,
+  idiomasOficiales: List<String>
+){
+
+}
