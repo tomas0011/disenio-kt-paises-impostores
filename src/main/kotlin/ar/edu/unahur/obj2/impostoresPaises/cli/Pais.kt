52c7437cd811d558016ac7249e32be5aea494e8d
LautaroRiofrio <riofriolautaro@gmail.com>
2022-07-03 18:47:38 -0300

    se agregaron los metodos a la clase pais

--- src/main/kotlin/ar/edu/unahur/obj2/impostoresPaises/cli/Pais.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/impostoresPaises/cli/Pais.kt
@@ -1,14 +1,34 @@
 package ar.edu.unahur.obj2.impostoresPaises.cli
 
+import kotlin.math.roundToInt
+
 class Pais (
-  nombre: String,
-  codigoiso3: String,
-  poblacion: Int,
-  superficie: Double,
-  continente: String,
-  codigoMoneda: String,
-  cotizacionDolar: Double,
-  paisesLimitrofes: List<Pais>,
-  bloquesRegionales: List<String>,
-  idiomasOficiales: List<String>
-){}
+  private val nombre: String,
+  private val codigoiso3: String,
+  private val poblacion: Int,
+  private val superficie: Double,
+  private val continente: String,
+  private val codigoMoneda: String,
+  private val cotizacionDolar: Double,
+  private val paisesLimitrofes: List<Pais>,
+  private val bloquesRegionales: List<String>,
+  private val idiomasOficiales: List<String>
+){
+  fun esPlurinacional() = idiomasOficiales.size > 1
+
+  fun esUnaIsla() = paisesLimitrofes.isEmpty()
+
+  fun densidadPoblacional() = (poblacion * superficie).roundToInt()
+
+  fun vecinoMasPoblado(): String {
+    var paisMasPoblado = nombre
+    var poblacionDelMasPoblado = poblacion
+    paisesLimitrofes.map{
+      if(it.poblacion > poblacionDelMasPoblado){
+        paisMasPoblado = it.nombre
+        poblacionDelMasPoblado = it.poblacion
+      }
+    }
+    return paisMasPoblado
+  }
+}
