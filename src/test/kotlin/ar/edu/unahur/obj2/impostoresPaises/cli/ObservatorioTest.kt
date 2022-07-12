27e94a320a4546249cacfe5baef15ab8824df1de
Tomas <tomasbenjamin117@gmail.com>
2022-07-11 21:40:40 -0300

    estructurar testing

--- src/test/kotlin/ar/edu/unahur/obj2/impostoresPaises/cli/ObservatorioTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/impostoresPaises/cli/ObservatorioTest.kt
@@ -5,77 +5,98 @@ import io.kotest.matchers.booleans.shouldBeTrue
 import io.kotest.matchers.shouldBe
 
 class ObservatorioTest: DescribeSpec({
-    describe("metodos de conjunto") {
-        describe("continenteConMasPlurinacionales") {
+    beforeEach {
+        val Argentina = Pais(
+            "Argentina",
+            "ARG",
+            47000000,
+            100.0,
+            "América",
+            "ARS",
+            235.0,
+            listOf(BloquesRegionales.MERCOSUR, BloquesRegionales.MCCA,BloquesRegionales.CARICOM, BloquesRegionales.ALADI),
+            listOf(Idiomas.Español)
+        )
 
-            beforeEach {
-                var Chile: Pais? = null
-                var Brasil: Pais? = null
-                var Uruguay: Pais? = null
+        val Uruguay = Pais(
+            "Uruguay",
+            "UR",
+            3400000,
+            100.0,
+            "América",
+            "Uru",
+            39.47,
+            listOf(BloquesRegionales.MERCOSUR, BloquesRegionales.TLC),
+            listOf(Idiomas.Español)
+        )
 
-                val Argentina = Pais(
-                    "Argentina",
-                    "ARG",
-                    47000000,
-                    100.0,
-                    "América",
-                    "ARS",
-                    235.0,
-                    listOf(Chile, Brasil, Uruguay) as List<Pais>,
-                    listOf("MERCOSUR", "MCCA", "CARICOM", "ALADI"),
-                    listOf("Español")
-                )
+        val Brasil = Pais(
+            "Brasil",
+            "BR",
+            212600000,
+            100.0,
+            "América",
+            "Br",
+            5.33,
+            listOf(BloquesRegionales.MERCOSUR),
+            listOf(Idiomas.Portugues)
+        )
 
-                Uruguay = Pais(
-                    "Uruguay",
-                    "UR",
-                    3400000,
-                    100.0,
-                    "América",
-                    "Uru",
-                    39.47,
-                    listOf(Argentina, Brasil) as List<Pais>,
-                    listOf("MERCOSUR", "TLC"),
-                    listOf("Español")
-                )
+        val Chile = Pais(
+            "Chile",
+            "CH",
+            19200000,
+            100.0,
+            "América",
+            "Chi",
+            932.58,
+            listOf(BloquesRegionales.UNASUR, BloquesRegionales.CELAC),
+            listOf(Idiomas.Español)
+        )
 
-                Brasil = Pais(
-                    "Brasil",
-                    "BR",
-                    212600000,
-                    100.0,
-                    "América",
-                    "Br",
-                    5.33,
-                    listOf(Argentina, Uruguay) as List<Pais>,
-                    listOf("MERCOSUR"),
-                    listOf("Portugues")
-                )
+        Argentina.addPaisesLimitrofes(listOf(Chile, Brasil, Uruguay))
+        Uruguay.addPaisesLimitrofes(listOf(Argentina, Brasil))
+        Brasil.addPaisesLimitrofes(listOf(Argentina, Uruguay))
+        Chile.addPaisesLimitrofes(listOf(Argentina))
+    }
 
-                Chile = Pais(
-                    "Chile",
-                    "CH",
-                    19200000,
-                    100.0,
-                    "América",
-                    "Chi",
-                    932.58,
-                    listOf(Argentina),
-                    listOf("UNASUR", "CELAC"),
-                    listOf("Español")
-                )
+    describe("metodos de observatorio") {
+        describe("sonLimítrofes") {
 
-                observatorio.agregarPais(Argentina)
-                observatorio.agregarPais(Uruguay!!)
-                observatorio.agregarPais(Brasil!!)
-                observatorio.agregarPais(Chile!!)
-            }
+        }
 
+        describe("necesitanTraducción") {
+
+        }
+
+        describe("sonPotencialesAliados") {
+
+        }
+
+        describe("convieneIrDeCompras") {
+
+        }
+
+        describe("aCuántoEquivale") {
+
+        }
+    }
+
+    describe("metodos de conjunto de observatorio") {
+        describe("obtener5TopPaises") {
             it("deberia devolver al de mayor densidad primero") {
                 val top5Paises = observatorio.obtener5TopPaises()
                 (top5Paises[0].densidadPoblacional() >= top5Paises[1].densidadPoblacional()).shouldBeTrue()
                 (top5Paises[0].densidadPoblacional() >= top5Paises[2].densidadPoblacional()).shouldBeTrue()
             }
+        }
+
+        describe("continenteConMasPlurinacionales") {
+
+        }
+
+        describe("conocerPromedioPoblacionalInsular") {
+            
         }
     }
 })
