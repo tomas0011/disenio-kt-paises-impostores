e81bdaaa3fc2094775367b25c8b3d5b19bd8759b
LautaroRiofrio <riofriolautaro@gmail.com>
2022-07-11 22:02:35 -0300

    test de metodos de observatorio

--- src/test/kotlin/ar/edu/unahur/obj2/impostoresPaises/cli/ObservatorioTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/impostoresPaises/cli/ObservatorioTest.kt
@@ -1,88 +1,147 @@
 package ar.edu.unahur.obj2.impostoresPaises.cli
 
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.booleans.shouldBeFalse
 import io.kotest.matchers.booleans.shouldBeTrue
 import io.kotest.matchers.shouldBe
 
 class ObservatorioTest: DescribeSpec({
-    beforeEach {
-        val Argentina = Pais(
-            "Argentina",
-            "ARG",
-            47000000,
-            100.0,
-            "América",
-            "ARS",
-            235.0,
-            listOf(BloquesRegionales.MERCOSUR, BloquesRegionales.MCCA,BloquesRegionales.CARICOM, BloquesRegionales.ALADI),
-            listOf(Idiomas.Español)
-        )
+        //beforeEach {
+            val Argentina = Pais(
+                "Argentina",
+                "ARG",
+                47000000,
+                100.0,
+                "América",
+                "ARS",
+                235.0,
+                listOf(BloquesRegionales.MERCOSUR,
+                    BloquesRegionales.MCCA,
+                    BloquesRegionales.CARICOM,
+                    BloquesRegionales.ALADI),
+                listOf(Idiomas.Español)
+            )
 
-        val Uruguay = Pais(
-            "Uruguay",
-            "UR",
-            3400000,
-            100.0,
-            "América",
-            "Uru",
-            39.47,
-            listOf(BloquesRegionales.MERCOSUR, BloquesRegionales.TLC),
-            listOf(Idiomas.Español)
-        )
+            val Uruguay = Pais(
+                "Uruguay",
+                "UR",
+                3400000,
+                100.0,
+                "América",
+                "Uru",
+                39.47,
+                listOf(BloquesRegionales.MERCOSUR, BloquesRegionales.TLC),
+                listOf(Idiomas.Español)
+            )
 
-        val Brasil = Pais(
-            "Brasil",
-            "BR",
-            212600000,
-            100.0,
-            "América",
-            "Br",
-            5.33,
-            listOf(BloquesRegionales.MERCOSUR),
-            listOf(Idiomas.Portugues)
-        )
+            val Brasil = Pais(
+                "Brasil",
+                "BR",
+                212600000,
+                100.0,
+                "América",
+                "Br",
+                5.33,
+                listOf(BloquesRegionales.MERCOSUR),
+                listOf(Idiomas.Portugues)
+            )
 
-        val Chile = Pais(
-            "Chile",
-            "CH",
-            19200000,
-            100.0,
-            "América",
-            "Chi",
-            932.58,
-            listOf(BloquesRegionales.UNASUR, BloquesRegionales.CELAC),
-            listOf(Idiomas.Español)
-        )
+            val Chile = Pais(
+                "Chile",
+                "CH",
+                19200000,
+                100.0,
+                "América",
+                "Chi",
+                932.58,
+                listOf(BloquesRegionales.UNASUR, BloquesRegionales.CELAC),
+                listOf(Idiomas.Español)
+            )
 
-        Argentina.addPaisesLimitrofes(listOf(Chile, Brasil, Uruguay))
-        Uruguay.addPaisesLimitrofes(listOf(Argentina, Brasil))
-        Brasil.addPaisesLimitrofes(listOf(Argentina, Uruguay))
-        Chile.addPaisesLimitrofes(listOf(Argentina))
-    }
+            Argentina.addPaisesLimitrofes(listOf(Chile, Brasil, Uruguay))
+            Uruguay.addPaisesLimitrofes(listOf(Argentina, Brasil))
+            Brasil.addPaisesLimitrofes(listOf(Argentina, Uruguay))
+            Chile.addPaisesLimitrofes(listOf(Argentina))
+        //}
 
-    describe("metodos de observatorio") {
-        describe("sonLimítrofes") {
+        describe("metodos de observatorio") {
+            describe("sonLimítrofes") {
+                it("Brasil deberia limitar con Argentina") {
+                    observatorio.sonLimítrofes(Argentina, Brasil).shouldBeTrue()
+                }
+                it("Argentina deberia limitar con Brasil") {
+                    observatorio.sonLimítrofes(Brasil,Argentina).shouldBeTrue()
+                }
+                it("Chile no deberia limitar con Uruguay") {
+                    observatorio.sonLimítrofes(Uruguay,Chile).shouldBeFalse()
+                }
+            }
 
+            describe("necesitanTraducción") {
+                it("Argentina no deberia necesitar traduccion con Chile"){
+                    observatorio.necesitanTraducción(Argentina, Chile).shouldBeFalse()
+                }
+                it("Argentina no deberia necesitar traduccion con Uruguay"){
+                    observatorio.necesitanTraducción(Argentina, Uruguay).shouldBeFalse()
+                }
+                it("Argentina deberia necesitar traduccion con Brasil"){
+                    observatorio.necesitanTraducción(Argentina, Brasil).shouldBeTrue()
+                }
+            }
+
+            describe("sonPotencialesAliados") {
+                it("Chile no deberia ser potencial Aliado de Brasil"){
+                    observatorio.sonPotencialesAliados(Brasil, Chile).shouldBeFalse()
+                }
+                it("Uruguay no deberia ser potencial aliado de Chile"){
+                    observatorio.sonPotencialesAliados(Uruguay, Chile).shouldBeFalse()
+                }
+                it("Argentina deberia ser potencial aliado de Uruguay"){
+                    observatorio.sonPotencialesAliados(Uruguay, Argentina).shouldBeTrue()
+                }
+                it("Argentina no deberia ser potencial aliado de Brasil"){
+                    observatorio.sonPotencialesAliados(Argentina,Brasil).shouldBeFalse()
+                }
+            }
+
+            describe("convieneIrDeCompras") {
+                describe("A Brasil le conviene ir de compras a todos los paises "){
+                    it("A Brasil deberia convenirle ir de compras a Argentina"){
+                        observatorio.convieneIrDeCompras(Brasil,Argentina).shouldBeTrue()
+                    }
+                    it("A Brasil deberia convenirle ir de compras a Uruguay"){
+                        observatorio.convieneIrDeCompras(Brasil,Uruguay).shouldBeTrue()
+                    }
+                    it("A Brasil deberia convenirle ir de compras a Chile"){
+                        observatorio.convieneIrDeCompras(Brasil,Chile).shouldBeTrue()
+                    }
+                }
+
+                describe("A Chile no deberia convenirle ir a comprar a ningun pais"){
+                    it("A Chile no deberia convenirle ir de compras a Argentina"){
+                        observatorio.convieneIrDeCompras(Chile,Argentina).shouldBeFalse()
+                    }
+                    it("A Chile no deberia convenirle ir de compras a Brasil"){
+                        observatorio.convieneIrDeCompras(Chile, Brasil).shouldBeFalse()
+                    }
+                    it("A Chile no deberia convenirle ir de compras a Uruguay"){
+                        observatorio.convieneIrDeCompras(Chile, Uruguay).shouldBeFalse()
+                    }
+                }
+            }
+
+            describe("aCuántoEquivale") {
+                it("100 pesos argentinos deberian ser 396.84"){
+                    observatorio.aCuántoEquivale(Argentina, Chile, 100.0).shouldBe(396.84)
+                }
+
+                it("5 reales deberian ser 37.02 pesos Uruguayos"){
+                    observatorio.aCuántoEquivale(Brasil, Uruguay, 5.0).shouldBe(37.03)
+                }
+            }
         }
 
-        describe("necesitanTraducción") {
-
-        }
-
-        describe("sonPotencialesAliados") {
-
-        }
-
-        describe("convieneIrDeCompras") {
-
-        }
-
-        describe("aCuántoEquivale") {
-
-        }
-    }
-
-    describe("metodos de conjunto de observatorio") {
+        /*describe("metodos de conjunto de observatorio") {
         describe("obtener5TopPaises") {
             it("deberia devolver al de mayor densidad primero") {
                 val top5Paises = observatorio.obtener5TopPaises()
@@ -98,5 +157,6 @@ class ObservatorioTest: DescribeSpec({
         describe("conocerPromedioPoblacionalInsular") {
             
         }
-    }
+    } */
+
 })
